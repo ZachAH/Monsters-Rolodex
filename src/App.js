7d26dc62b265a4b1608fe35ee477078fb631d1a9
ZMAN <zachh@github.com>
2021-03-05 11:37:12 -0600

    added card css and card-list-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list-component';
 
 class App extends Component {
   constructor() {
@@ -9,23 +10,24 @@ class App extends Component {
       monsters: []
     };
   }
-    //fetching api then converting it to json and then setting it as the monster array for users
+  //fetching api then converting it to json and then setting it as the monster array for users
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
-      
-      
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+
+
   }
 
 
   render() {
     return (
       <div className='App'>
-        {
-          this.state.monsters.map(monster => (
+        <CardList name='Yihua'>
+          {this.state.monsters.map(monster => (
             <h1 key={monster.id}> {monster.name} </h1>
           ))}
+        </CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list-component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './card-list.styles.css';
+
+export const CardList = props => {
+
+    return <div class='card-list'>{props.children}</div>
+};
+
